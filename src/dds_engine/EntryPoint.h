85dd92b04ad13049fb109d51018a3069c2197799
idiasdas <igordias.ti@gmail.com>
2024-04-05 08:57:06 +0200

    Using Log on entry point

--- src/dds_engine/EntryPoint.h
+++ src/dds_engine/EntryPoint.h
@@ -5,6 +5,8 @@ extern dds::Application* dds::CreateApplication();
 int main(int argc, char** argv) {
 
     dds::Log::Init();
+    dds::Log::GetCoreLogger()->info("Engine Initialized");
+    dds::Log::GetClientLogger()->info("Client Initialized");
 
     std::cout << "Creating Client Application" << std::endl;
     auto app = dds::CreateApplication();
