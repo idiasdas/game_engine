b9d67b7aa48b4ace9934157f935c69064929edce
idiasdas <igordias.ti@gmail.com>
2024-04-06 13:36:30 +0200

    Removed std::cout use

--- src/dds_engine/EntryPoint.h
+++ src/dds_engine/EntryPoint.h
@@ -3,12 +3,11 @@
 extern dds::Application* dds::CreateApplication();
 
 int main(int argc, char** argv) {
-
     dds::Log::Init();
+
     DDS_CORE_INFO("Engine Initialized");
     DDS_INFO("Client Initialized");
 
-    std::cout << "Creating Client Application" << std::endl;
     auto app = dds::CreateApplication();
     app->Run();
     delete app;
