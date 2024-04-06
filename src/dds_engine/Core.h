d6be888cb4de3dafe7faca2bf981aa8e17b337e9
idiasdas <igordias.ti@gmail.com>
2024-04-06 13:36:56 +0200

    Added platform definitions check for Windows and Linux

--- src/dds_engine/Core.h
+++ src/dds_engine/Core.h
@@ -1,9 +1,20 @@
 #pragma once
-
-#ifdef DDS_BUILD_LIB
-    #define DDS_API __attribute__((visibility("default")))
+#ifdef DDS_PLATFORM_WINDOWS
+    #ifdef DDS_BUILD_LIB
+        #define DDS_API __declspec(dllexport)
+    #else
+        #define DDS_API __declspec(dllimport)
+    #endif
 #else
-    #define DDS_API
+    #ifdef DDS_PLATFORM_LINUX
+        #ifdef DDS_BUILD_LIB
+            #define DDS_API __attribute__((visibility("default")))
+        #else
+            #define DDS_API
+        #endif
+    #else
+        #error "Platform not supported"
+    #endif
 #endif
 
 #define BIT(x) (1 << x)
