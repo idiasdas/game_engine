213c2f7f9c6d0a406f0602e20f06637d9605c927
idiasdas <igordias.ti@gmail.com>
2024-04-06 23:13:52 +0200

    Added DDS_ASSERT and DDS_CORE_ASSERT for Linux with the precompiled definition DDS_ENABLE_ASSERTS

--- src/dds_engine/Core.h
+++ src/dds_engine/Core.h
@@ -17,4 +17,14 @@
     #endif
 #endif
 
+
+#ifdef DDS_ENABLE_ASSERTS
+    #define DDS_ASSERT(x, ...) { if(!(x)) { DDS_ERROR("Assertion Failed: {0}", __VA_ARGS__); __asm__ __volatile__("int3"); } }
+    #define DDS_CORE_ASSERT(x, ...) { if(!(x)) { DDS_CORE_ERROR("Assertion Failed: {0}", __VA_ARGS__); __asm__ __volatile__("int3"); } }
+#else
+    #define DDS_ASSERT(x, ...)
+    #define DDS_CORE_ASSERT(x, ...)
+#endif
+
+
 #define BIT(x) (1 << x)
