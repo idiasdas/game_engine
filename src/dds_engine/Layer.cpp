b9d5e7ce5f0fa2178dae582bf8b2544439072432
idiasdas <igordias.ti@gmail.com>
2024-04-07 14:49:26 +0200

    Created Layer and LaayerStack

---
+++ src/dds_engine/Layer.cpp
@@ -0,0 +1,14 @@
+#include "dds_enginepch.h"
+#include "Layer.h"
+
+namespace dds {
+    Layer::Layer(const std::string& name)
+        : m_DebugName(name)
+    {
+    }
+
+    Layer::~Layer()
+    {
+
+    }
+}
