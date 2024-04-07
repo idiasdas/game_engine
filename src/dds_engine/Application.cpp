48ac12385bc4833f2730fafa4780be32efd09c5c
idiasdas <igordias.ti@gmail.com>
2024-04-07 12:37:12 +0200

    Tracing Window Events and disptching WindowCloseEvent

--- src/dds_engine/Application.cpp
+++ src/dds_engine/Application.cpp
@@ -1,19 +1,30 @@
 #include "dds_enginepch.h"
 #include "Application.h"
-#include "Events/ApplicationEvent.h"
 
 #include <GLFW/glfw3.h>
 
 namespace dds{
 
+#define BIND_EVENT_FN(x) std::bind(x, this, std::placeholders::_1)
+
     Application::Application()
     {
         m_Window = std::unique_ptr<Window>(Window::Create());
+        m_Window->SetEventCallBack(BIND_EVENT_FN(&Application::OnEvent));
     }
 
     Application::~Application()
     {
     }
+
+
+    void Application::OnEvent(Event& e)
+    {
+        EventDispatcher dispatcher(e);
+        dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(&Application::OnWindowClose));
+        DDS_CORE_TRACE("{0}", e);
+    }
+
 
     void Application::Run()
     {
@@ -26,4 +37,11 @@ namespace dds{
             m_Window->OnUpdate();
         }
     }
+
+    bool Application::OnWindowClose(WindowCloseEvent& e)
+    {
+        m_Running = false;
+        return true;
+    }
+
 }
--- src/dds_engine/Application.h
+++ src/dds_engine/Application.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
+
 #include "Window.h"
 
 namespace dds{
@@ -13,9 +15,14 @@ namespace dds{
         virtual ~Application();
 
         void Run();
+
+        void OnEvent(Event& e);
     private:
+        bool OnWindowClose(WindowCloseEvent& e);
+
         std::unique_ptr<Window> m_Window;
         bool m_Running = true;
+
     };
     // Defined in the client application
     Application*  CreateApplication();
