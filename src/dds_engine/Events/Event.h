bf87a0757fe02509df06b9ee399df7f6fd617a74
idiasdas <igordias.ti@gmail.com>
2024-04-07 14:50:04 +0200

    Fixed missing methods in Event

--- src/dds_engine/Events/Event.h
+++ src/dds_engine/Events/Event.h
@@ -39,14 +39,20 @@ namespace dds {
         friend class EventDispatcher;
 
     public:
+        virtual ~Event() = default;
+
+        bool Handled  = false;
+
         virtual EventType GetEventType() const = 0;
         virtual const char* GetName() const = 0;
         virtual int GetCategoryFlags() const = 0;
         virtual std::string ToString() const { return GetName(); }
 
-        inline bool IsInCategory(EventCategory category) {
+        inline bool IsInCategory(EventCategory category)
+        {
             return GetCategoryFlags() & category;
         }
+
     protected:
         bool m_Handled = false;
     };
