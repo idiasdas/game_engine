7584789a29a5a3076b01b8ad5a8bd964f174c272
idiasdas <igordias.ti@gmail.com>
2024-04-13 23:40:00 +0200

    Handling events for ImGuiLayer

--- dds_engine/src/dds_engine/ImGui/ImGuiLayer.cpp
+++ dds_engine/src/dds_engine/ImGui/ImGuiLayer.cpp
@@ -1,10 +1,13 @@
 #include "dds_enginepch.h"
 #include "ImGuiLayer.h"
 
-#include <GLFW/glfw3.h>
 #include "imgui.h"
 #include "Platform/OpenGL/ImGuiOpenGLRenderer.h"
 #include "dds_engine/Application.h"
+
+// TEMPORARY INCLUDES
+#include <GLFW/glfw3.h>
+#include <glad/glad.h>
 
 namespace dds{
 
@@ -59,7 +62,72 @@ namespace dds{
 
     void ImGuiLayer::OnEvent(Event& event)
     {
+        EventDispatcher dispatcher(event);
+        dispatcher.Dispatch<MouseButtonPressedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
+        dispatcher.Dispatch<MouseButtonReleasedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
+        dispatcher.Dispatch<MouseMovedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnMouseMovedEvent));
+        dispatcher.Dispatch<MouseScrolledEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnMouseScrolledEvent));
+        dispatcher.Dispatch<KeyPressedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
+        dispatcher.Dispatch<KeyReleasedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
+        // dispatcher.Dispatch<KeyTypedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
+        dispatcher.Dispatch<WindowResizeEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnWindowResizeEvent));
     }
 
+    bool ImGuiLayer::OnMouseButtonPressedEvent(MouseButtonPressedEvent &event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        io.MouseDown[event.GetMouseButton()] = true;
 
+        return false; // We want other layers to handle this event as well so we don't consume it here.
+    }
+
+    bool ImGuiLayer::OnMouseButtonReleasedEvent(MouseButtonReleasedEvent &event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        io.MouseDown[event.GetMouseButton()] = false;
+
+        return false;
+    }
+
+    bool ImGuiLayer::OnMouseMovedEvent(MouseMovedEvent &event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        io.MousePos = ImVec2(event.GetX(), event.GetY());
+
+        return false;
+    }
+
+    bool ImGuiLayer::OnMouseScrolledEvent(MouseScrolledEvent &event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        io.MouseWheel = event.GetYOffset();
+        io.MouseWheelH = event.GetXOffset();
+
+        return false;
+    }
+
+    bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent &event)
+    {
+        return false;
+    }
+
+    bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent &event)
+    {
+        return false;
+    }
+
+    // bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent &event)
+    // {
+    //     return false;
+    // }
+
+    bool ImGuiLayer::OnWindowResizeEvent(WindowResizeEvent& event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+        io.DisplaySize = ImVec2(event.GetWidth(), event.GetHeight());
+        io.DisplayFramebufferScale = ImVec2(1.0f, 1.0f);
+        glViewport(0, 0, event.GetWidth(), event.GetHeight());
+
+        return false;
+    }
 }
--- dds_engine/src/dds_engine/ImGui/ImGuiLayer.h
+++ dds_engine/src/dds_engine/ImGui/ImGuiLayer.h
@@ -1,6 +1,9 @@
 #pragma once
 
 #include "dds_engine/Layer.h"
+#include "dds_engine/Events/MouseEvent.h"
+#include "dds_engine/Events/KeyEvent.h"
+#include "dds_engine/Events/ApplicationEvent.h"
 
 namespace dds {
 
@@ -14,6 +17,15 @@ namespace dds {
         void OnDetach();
         void OnUpdate();
         void OnEvent(Event& event);
+    private:
+        bool OnMouseButtonPressedEvent(MouseButtonPressedEvent& event);
+        bool OnMouseButtonReleasedEvent(MouseButtonReleasedEvent& event);
+        bool OnMouseMovedEvent(MouseMovedEvent& event);
+        bool OnMouseScrolledEvent(MouseScrolledEvent& event);
+        bool OnKeyPressedEvent(KeyPressedEvent& event);
+        bool OnKeyReleasedEvent(KeyReleasedEvent& event);
+        // bool OnKeyTypedEvent(KeyTypedEvent& event); // TODO: Create KeyTypedEvent
+        bool OnWindowResizeEvent(WindowResizeEvent& event);
     private:
         float m_Time = 0.0f;
     };
