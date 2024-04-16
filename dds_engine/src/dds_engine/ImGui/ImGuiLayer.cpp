cab8b48836225cc7b3f207e74dfc00d0c57dfb26
idiasdas <igordias.ti@gmail.com>
2024-04-16 16:10:11 +0200

    Add KeyMap between ImGui and GLFW

--- dds_engine/src/dds_engine/ImGui/ImGuiLayer.cpp
+++ dds_engine/src/dds_engine/ImGui/ImGuiLayer.cpp
@@ -29,7 +29,28 @@ namespace dds{
         io.BackendFlags |= ImGuiBackendFlags_HasMouseCursors;
         io.BackendFlags |= ImGuiBackendFlags_HasSetMousePos;
 
-        // io.KeyMap
+        // TEMPORARY io.KeyMap
+        io.KeyMap[ImGuiKey_Tab] = GLFW_KEY_TAB;
+        io.KeyMap[ImGuiKey_LeftArrow] = GLFW_KEY_LEFT;
+        io.KeyMap[ImGuiKey_RightArrow] = GLFW_KEY_RIGHT;
+        io.KeyMap[ImGuiKey_UpArrow] = GLFW_KEY_UP;
+        io.KeyMap[ImGuiKey_DownArrow] = GLFW_KEY_DOWN;
+        io.KeyMap[ImGuiKey_PageUp] = GLFW_KEY_PAGE_UP;
+        io.KeyMap[ImGuiKey_PageDown] = GLFW_KEY_PAGE_DOWN;
+        io.KeyMap[ImGuiKey_Home] = GLFW_KEY_HOME;
+        io.KeyMap[ImGuiKey_End] = GLFW_KEY_END;
+        io.KeyMap[ImGuiKey_Insert] = GLFW_KEY_INSERT;
+        io.KeyMap[ImGuiKey_Delete] = GLFW_KEY_DELETE;
+        io.KeyMap[ImGuiKey_Backspace] = GLFW_KEY_BACKSPACE;
+        io.KeyMap[ImGuiKey_Space] = GLFW_KEY_SPACE;
+        io.KeyMap[ImGuiKey_Enter] = GLFW_KEY_ENTER;
+        io.KeyMap[ImGuiKey_Escape] = GLFW_KEY_ESCAPE;
+        io.KeyMap[ImGuiKey_A] = GLFW_KEY_A;
+        io.KeyMap[ImGuiKey_C] = GLFW_KEY_C;
+        io.KeyMap[ImGuiKey_V] = GLFW_KEY_V;
+        io.KeyMap[ImGuiKey_X] = GLFW_KEY_X;
+        io.KeyMap[ImGuiKey_Y] = GLFW_KEY_Y;
+        io.KeyMap[ImGuiKey_Z] = GLFW_KEY_Z;
 
         ImGui_ImplOpenGL3_Init("#version 410");
     }
@@ -69,7 +90,7 @@ namespace dds{
         dispatcher.Dispatch<MouseScrolledEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnMouseScrolledEvent));
         dispatcher.Dispatch<KeyPressedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyPressedEvent));
         dispatcher.Dispatch<KeyReleasedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyReleasedEvent));
-        // dispatcher.Dispatch<KeyTypedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
+        dispatcher.Dispatch<KeyTypedEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnKeyTypedEvent));
         dispatcher.Dispatch<WindowResizeEvent>(DDS_BIND_EVENT_FN(ImGuiLayer::OnWindowResizeEvent));
     }
 
@@ -108,18 +129,33 @@ namespace dds{
 
     bool ImGuiLayer::OnKeyPressedEvent(KeyPressedEvent &event)
     {
+        ImGuiIO& io = ImGui::GetIO();
+        io.KeysDown[event.GetKeyCode()] = true;
+
+        io.KeyCtrl = io.KeysDown[GLFW_KEY_LEFT_CONTROL] || io.KeysDown[GLFW_KEY_RIGHT_CONTROL];
+        io.KeyShift = io.KeysDown[GLFW_KEY_LEFT_SHIFT] || io.KeysDown[GLFW_KEY_RIGHT_SHIFT];
+        io.KeyAlt = io.KeysDown[GLFW_KEY_LEFT_ALT] || io.KeysDown[GLFW_KEY_RIGHT_ALT];
+        io.KeySuper = io.KeysDown[GLFW_KEY_LEFT_SUPER] || io.KeysDown[GLFW_KEY_RIGHT_SUPER];
+
         return false;
     }
 
     bool ImGuiLayer::OnKeyReleasedEvent(KeyReleasedEvent &event)
     {
+        ImGuiIO& io = ImGui::GetIO();
+        io.KeysDown[event.GetKeyCode()] = false;
+
         return false;
     }
 
-    // bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent &event)
-    // {
-    //     return false;
-    // }
+    bool ImGuiLayer::OnKeyTypedEvent(KeyTypedEvent &event)
+    {
+        ImGuiIO& io = ImGui::GetIO();
+
+        io.AddInputCharacter(event.GetKeyCode());
+
+        return false;
+    }
 
     bool ImGuiLayer::OnWindowResizeEvent(WindowResizeEvent& event)
     {
