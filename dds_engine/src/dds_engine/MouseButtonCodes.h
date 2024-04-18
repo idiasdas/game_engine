1be71fe57d01e50f461bfdde4a917dda8ba41ed5
idiasdas <igordias.ti@gmail.com>
2024-04-18 19:56:07 +0200

    Got Key and Mouse codes from GLFW

---
+++ dds_engine/src/dds_engine/MouseButtonCodes.h
@@ -0,0 +1,15 @@
+#pragma once
+
+// From glfw3.h
+#define DDS_MOUSE_BUTTON_1         0
+#define DDS_MOUSE_BUTTON_2         1
+#define DDS_MOUSE_BUTTON_3         2
+#define DDS_MOUSE_BUTTON_4         3
+#define DDS_MOUSE_BUTTON_5         4
+#define DDS_MOUSE_BUTTON_6         5
+#define DDS_MOUSE_BUTTON_7         6
+#define DDS_MOUSE_BUTTON_8         7
+#define DDS_MOUSE_BUTTON_LAST      DDS_MOUSE_BUTTON_8
+#define DDS_MOUSE_BUTTON_LEFT      DDS_MOUSE_BUTTON_1
+#define DDS_MOUSE_BUTTON_RIGHT     DDS_MOUSE_BUTTON_2
+#define DDS_MOUSE_BUTTON_MIDDLE    DDS_MOUSE_BUTTON_3
