cdd1438bc9d8ebc67156f3ac8b12dc905e7babee
idiasdas <igordias.ti@gmail.com>
2024-04-18 19:57:09 +0200

    Included Input and Key codes to DDS Engine header

--- dds_engine/src/dds_engine.h
+++ dds_engine/src/dds_engine.h
@@ -4,8 +4,12 @@
 #include "dds_engine/Application.h"
 #include "dds_engine/Layer.h"
 #include "dds_engine/Log.h"
-#include "dds_engine/ImGui/ImGuiLayer.h"
 
+#include "dds_engine/Input.h"
+#include "dds_engine/KeyCodes.h"
+#include "dds_engine/MouseButtonCodes.h"
+
+#include "dds_engine/ImGui/ImGuiLayer.h"
 // --------------------------------------
 // Entry point for the client application
 #include "dds_engine/EntryPoint.h"
