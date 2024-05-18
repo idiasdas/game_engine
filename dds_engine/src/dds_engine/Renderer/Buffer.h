f320d2f6b9115bbe4ad324427f76b0f3365f7ed4
idiasdas <igordias.ti@gmail.com>
2024-05-19 01:10:43 +0200

    [BUG-FIX] Add missing const modifier

--- dds_engine/src/dds_engine/Renderer/Buffer.h
+++ dds_engine/src/dds_engine/Renderer/Buffer.h
@@ -83,7 +83,7 @@ namespace dds
             CalculateOffsetAndStride();
         }
 
-        inline const std::vector<BufferElement> &GetElements() { return m_Elements; }
+        inline const std::vector<BufferElement> &GetElements() const { return m_Elements; }
         inline const uint32_t &GetStride() const { return m_Stride; }
 
         std::vector<BufferElement>::iterator begin() { return m_Elements.begin(); }
